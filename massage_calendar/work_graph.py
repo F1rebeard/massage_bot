f122b6901cfc94cfb070fbdc894091c76d8dc433
Александр Саратов <alxsrtv@gmail.com>
2024-03-01 14:29:05 +0300

    time to calendar

--- massage_calendar/work_graph.py
+++ massage_calendar/work_graph.py
@@ -4,13 +4,20 @@ import logging
 from create_bot import db
 
 
-async def get_working_hours_for_calendar():
+async def get_all_working_hours_and_days_off() -> [list, list]:
+    """
+    Return a list of all days off for a month and list of working hours for
+    each master for each weekday.
+    :return: master_work_hours = [['0', '0', '0', '0', '0', '0', '0'],
+    ['0', [(time(9, 0), time(15, 0))], [(time(2, 0), time(3, 0))]]
+    days_off_sum = [2, 6, 15, 31]
+    """
     masters_graphics = await db.get_all_masters_work_time()
     # total days off for all masters
     days_off_sum = []
+    all_masters_work_time = []
     # getting data for each master
     for master in masters_graphics:
-        master_id = master[0]
         if master[8] is not None:
             days_off = master[8].split(', ')
         else:
@@ -36,6 +43,61 @@ async def get_working_hours_for_calendar():
                     end_time = datetime.time(end)
                     day_graphic.append((start_time, end_time))
                 weekdays_graphic.append(day_graphic)
-        logging.info(f'master_id: {master_id}\n'
-                     f'weekdays_grapgic: {weekdays_graphic}\n'
-                     f'days_off: {days_off}')
+        all_masters_work_time.append(weekdays_graphic)
+    logging.info(f'all masters worktime: {all_masters_work_time}'
+                 f'all days off: {days_off_sum}')
+    return all_masters_work_time, days_off_sum
+
+
+def consolidate_intervals(intervals):
+    # Sort intervals by start time
+    intervals.sort(key=lambda x: x[0])
+    consolidated = [intervals[0]]
+    for current in intervals[1:]:
+        prev = consolidated[-1]
+        # If current overlaps prev, merge them
+        if current[0] <= prev[1]:
+            prev[1] = max(prev[1], current[1])
+        # Else just append non-overlapping current interval
+        else:
+            consolidated.append(current)
+    return consolidated
+
+
+async def consolidated_work_weekday_graphic_for_calendar(
+        all_masters_weekday_graphic: list, all_days_off: list
+) -> None:
+    """
+
+    :param all_masters_weekday_graphic:
+    :param all_days_off:
+    :return:
+    """
+# Consolidated calendar
+calendar = {
+    1: [],  # Monday
+    2: [],  # Tuesday
+    3: [],  # etc...
+}
+
+for master in master_work_hours:
+
+    for weekday, hours in enumerate(master):
+
+        if not hours or hours == '0':
+            continue
+
+        # If first time seeing this weekday, initialize empty list
+        if not calendar[weekday + 1]:
+            calendar[weekday + 1] = []
+
+        for start, end in hours:
+            calendar[weekday + 1].append((start, end))
+
+# Consolidate intervals
+for weekday in calendar:
+    calendar[weekday] = consolidate_intervals(calendar[weekday])
+
+print(calendar)
+
+
